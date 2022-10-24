d234faf1e11a7c11d2c527bef8167919d0804f5c
AlexisGmez <alexisgmez65@gmail.com>
2022-10-24 18:36:28 -0500

    adding products to dom dynamic

--- code.js
+++ code.js
@@ -1,4 +1,31 @@
 "use strict";
+const products = [
+    {
+    id: 1,
+    name: 'Hoodies',
+    price: 14.00,
+    image: 'assets/images/featured1.png',
+    category: 'hoodies',
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: 'Shirts',
+    price: 24.00,
+    image: 'assets/images/featured2.png',
+    category: 'shirts',
+    quantity: 15
+  },
+  {
+    id: 3,
+    name: 'Sweatshirts',
+    price: 24.00,
+    image: 'assets/images/featured3.png',
+    category: 'sweatshirts',
+    quantity: 20
+  }
+];
+const cartShopping=[];
 
 const darkMode = () =>{
 
@@ -48,10 +75,83 @@ const dropdownCart =()=>{
         shoppingcart.classList.toggle("active");
     });
     cart__count.addEventListener("click",e=>{
+        e.preventDefault();
         shoppingcart.classList.toggle("active");
     });
     
 }
+
+const addingProductsDom =()=>{
+    const container = document.querySelector(".main__products");
+    products.forEach(element => {
+        let html = `<div class="main__product ">
+                        <img src="${element.image}" alt="">
+                        <div class="product__description">
+                            <h3>$${element.price}</h3>
+                            <span>Stock: ${element.quantity}</span>
+                            <p class="text__color">${element.name}</p>
+                            <div class="icon__product"><i data-id=${element.id} class='bx bx-plus'></i></div>
+                        </div>
+                    </div> `;
+        container.innerHTML += html;
+    });
+
+}
+
+//pendiente
+const addingProductsToLocalStorage = () =>{
+    const buttons = document.querySelectorAll(".icon__product");  
+    buttons.forEach(button=>{
+        button.addEventListener("click", e=>{
+            e.preventDefault();
+            cartShopping.push(products.filter(item=>item.id===parseInt(e.target.getAttribute("data-id"))));  
+            
+        }); 
+    });
+}
+//pendiente
+const addingProductsCartDom =()=>{
+    const add__container = document.querySelector(".added__elements");
+    if (!cartShopping.length) {
+        add__container.innerHTML = `<div class="added__elements--empty">
+                                        <img src="assets/images/empty-cart.png" alt="empty cart">
+                                        <h2>Your cart is empty</h2>
+                                        <p>You can add items to your cart by clicking on the + button on the product page.</p>
+                                    </div> `;
+    }else{
+        // cartShopping.forEach(item=>{        
+        //     let html = `<div class="added__element">
+        //                     <div class="cart__image">
+        //                         <img src="${item[0].image}" alt="">
+        //                     </div>
+        //                     <div class="cart__content">
+        //                         <h4>${item[0].name}</h4>
+        //                         <p>Stock: ${item[0].quantity} | <span>${item[0].price}</span></p>
+        //                         <h5><span>Subtotal: 120.00</span></h5>
+        //                         <div class="cart__buttons">
+        //                             <div class="cart__button--add">
+        //                                 <div><i class='bx bx-minus'></i></div>
+        //                                 <div><p class="text__color">4 units</p></div>
+        //                                 <div><i class='bx bx-plus'></i></div>
+        //                             </div>
+        //                             <div class="cart__button--delete">
+        //                                 <span><i class='bx bx-trash' ></i></span>
+        //                             </div>
+    
+        //                         </div>
+        //                     </div>
+        //                 </div>`;
+        //     add__container.innerHTML+=html;
+        // });
+    }
+    
+}
+//pendiente
+const countItemsCart =()=>{
+    const count = document.querySelector(".cart__count");
+    
+}
+
 const swtichIcons =(element,classToEvaluate,icon,iconClassOne,iconClassTwo)=>{
 
     if (element.classList.contains(`${classToEvaluate}`)){
@@ -85,3 +185,6 @@ darkMode();
 dropdownMenu();
 dropdownCart();
 navPosition();
+addingProductsDom();
+addingProductsCartDom();
+
